9d0e0f4fb460e03984f6db673fa8d426ab1a01b7
강인서 <iskang0026@gmail.com>
2026-08-25 17:37:52 +0900

    feat: render weather mockup view
    - App.vue에서 기존 실습 컴포넌트 연결 정리
    - WeatherHomeView를 렌더링하도록 구성

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import SampleOne from './components/practices/basic/SampleOne.vue'
+// import SampleTwo from './components/practices/basic/SampleTwo.vue'
+import WeatherHomeView from './views/WeatherHomeView.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div style="padding: 20px">
+    <!-- <SampleOne /> -->
+    <!-- <SampleTwo /> -->
+    <WeatherHomeView />
+  </div>
 </template>
 
 <style scoped>
